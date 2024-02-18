255847769f2d9992f562a25537a36a5ceb56077c
BeniBiton <benbit33@gmail.com>
2024-02-18 17:22:45 +0200

    Saving some changes

---
+++ server/game.py
@@ -0,0 +1,21 @@
+"""
+handles operations related to game and connections between, players, board, chat and round
+"""
+class Game(object):
+    def __init__(self, id, players):
+        self.id = id
+        self.players = players
+        self.words_used = []
+        self.round = None
+        self.board = None
+    def player_guess(self, player, guess):
+        pass
+
+    def player_disconnected(self, player):
+        pass
+
+    def skip(self):
+        pass
+
+    def update_board(self):
+        pass
---
+++ server/round.py
@@ -0,0 +1,66 @@
+"""
+Represents a round of the game, storing things like word, time, skips, drawing player and more.
+"""
+import time as t
+from _thread import *
+from .game import Game
+from .chat import Chat
+
+class Round(object):
+    def __init__(self, word, player_drawing, players):
+        """
+        init object
+        :param word: str
+        :param player_drawing: Player
+        :param players: Player[]
+        """
+        self.word = word
+        self.player_drawing = player_drawing
+        self.player_guesses = []
+        self.skips = 0
+        self.player_scores = {player: 0 for player in players}
+        self.time = 75
+        self.chat = Chat()
+        start_new_thread(self.time_thread(), ())
+
+    def time_thread(self):
+        """
+        Runs in thread to keep track of time
+        :return: None
+        """
+        while self.time > 0:
+            t.sleep(1)
+            self.time -= 1
+        self.end_round("Time is up!")
+
+    def guess(self, player, wrd):
+        """
+        :returns bool if player got guess correct
+        :param player: Player
+        :param wrd:str
+        :return:bool
+        """
+        currect = wrd == self.word
+        if currect:
+            self.player_guesses.append(player)
+            # TODO implement scoring system here
+
+
+    def player_left(self, player):
+        """
+        removes player that left from scores and list
+        :param player: Player
+        :return: None
+        """
+        # might not be able to use player as key in dict
+        if player in self.player_scores:
+            del self.player_scores[player]
+        if player in self.player_guesses:
+            self.player_guesses.remove(player)
+        if player == self.player_drawing:
+            self.end_round("Drawing player leaves")
+
+
+    def end_round(self, msg):
+        # TODO implement end_round functionality
+        pass
